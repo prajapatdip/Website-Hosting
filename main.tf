22f3e0758e52987b29f7f00682eeebf03871aef5
prajapatdip <dipprajapat83@gmail.com>
2024-01-18 11:33:10 +0530

    Changes in main.tf and error.html

--- main.tf
+++ main.tf
@@ -9,6 +9,14 @@ resource "aws_s3_object" "index" {
   content_type = "text/html"
 }
 
+resource "aws_s3_bucket_ownership_controls" "host" {
+  bucket = aws_s3_bucket.mybucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
 resource "aws_s3_object" "error" {
   bucket = aws_s3_bucket.mybucket.id
   key = "error.html"
@@ -25,5 +33,85 @@ resource "aws_s3_bucket_website_configuration" "web" {
 
   error_document {
     key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "mybucket_acl" {
+  depends_on = [ aws_s3_bucket_ownership_controls.host ]
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "private"
+}
+
+locals {
+  s3_origin_id = "DipDevOpsOriginS3"
+}
+
+resource "aws_cloudfront_origin_access_identity" "my_origin_access_id" {
+  comment = "Some comment"
+}
+
+resource "aws_s3_bucket_policy" "allow_access" {
+  bucket = aws_s3_bucket.mybucket.id
+  policy = data.aws_iam_policy_document.allow_access.json
+}
+
+data "aws_iam_policy_document" "allow_access" {
+  depends_on = [ aws_cloudfront_distribution.cdn ]
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.mybucket.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.my_origin_access_id.iam_arn]
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name              = aws_s3_bucket.mybucket.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.my_origin_access_id.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "My cdn"
+  default_root_object = "index.html"
+
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
